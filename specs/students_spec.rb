81e864e878ba1b0c5ddf4c2ff563f1e4f027cc17
Brendan <laotsu@hotmail.co.uk>
2019-11-11 16:01:43 +0000

    Completed student exercise

--- specs/students_spec.rb
+++ specs/students_spec.rb
@@ -16,4 +16,26 @@ class TestStudent < MiniTest::Test
     student = Student.new("Brendan", "G16")
     assert_equal("G16", student.cohort)
   end
+
+  def test_set_name()
+    student = Student.new("Alan", "G16")
+    student.set_name("Alan Watt")
+    assert_equal("Alan Watt", student.name)
+  end
+
+  def test_set_cohort()
+    student = Student.new("Alan", "G15")
+    student.set_cohort("G16")
+    assert_equal("G16", student.cohort)
+  end
+
+  def test_student_talk()
+    student = Student.new("Alan", "G16")
+    assert_equal("I can talk!", student.talk("I can talk!"))
+  end
+
+  def test_fav_language()
+    student = Student.new("Alan", "G16")
+    assert_equal("I love Ruby!", student.fav_language("Ruby"))
+  end
 end
--- students.rb
+++ students.rb
@@ -13,4 +13,19 @@ class Student
     return @cohort
   end
 
+  def set_name(name)
+    @name = name
+  end
+
+  def set_cohort(cohort)
+    @cohort = cohort
+  end
+
+  def talk(talk)
+    return talk
+  end
+
+  def fav_language(language)
+    return "I love #{language}!" 
+  end
 end
